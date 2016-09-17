351fc4c23cc7b54c07f8a9a5aa9943b6151944e0
holyangel <slverwolf@gmail.com>
2016-09-17 11:17:00 -0500

    Revert "Change some appops"
    This reverts commit 191c17a1abd1d24d11bf6e0f8470d6128acd3f3e.

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -181,7 +181,6 @@ public class AppOpsState {
                     false,
                     false,
                     false,
-                    false,
                     false }
             );
 
